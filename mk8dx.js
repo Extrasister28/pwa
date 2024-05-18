505cb40341b853fdf99385e09c58c44a2e024492
MozukuDev <34203193+Extrasister28@users.noreply.github.com>
2024-05-18 15:17:40 +0900

    Update and rename mk8dxrandom.js to mk8dx.js

--- mk8dx.js
+++ mk8dx.js
@@ -2,7 +2,11 @@ function botanClick(){
     alert('ボタンがクリックされました');
 }
 
-let button = document.getElementById('btn');
+let button = document.getElementById('randbtn');
 
 // addEventListener( 'イベント', 処理)で要素にイベントが発火した際に処理を実行する
 button.addEventListener('click', botanClick);
+
+var img = document.getElementById(“image_file”);
+
+img.src = “imagemariokartcourse/1.png”;
