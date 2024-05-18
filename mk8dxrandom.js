831d4c64c398f303f69486d5ba848439a02d4b84
MozukuDev <34203193+Extrasister28@users.noreply.github.com>
2024-05-18 15:10:44 +0900

    Update mk8dxrandom.js

--- mk8dxrandom.js
+++ mk8dxrandom.js
@@ -1,1 +1,8 @@
+function botanClick(){
+    alert('ボタンがクリックされました');
+}
 
+let button = document.getElementById('btn');
+
+// addEventListener( 'イベント', 処理)で要素にイベントが発火した際に処理を実行する
+button.addEventListener('click', botanClick);
