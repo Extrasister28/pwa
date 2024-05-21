e45e7a4b4527151461bb53c0087e3e4f3e4de6e6
MozukuDev <34203193+Extrasister28@users.noreply.github.com>
2024-05-21 15:57:20 +0900

    Update mk8dx.js

--- mk8dx.js
+++ mk8dx.js
@@ -9,6 +9,10 @@ function botanClick(){
     for (let i = 0; i <= 30; i++) {
         (function(i){
             setTimeout(function() {
+                if (conditionList.length === 0) {
+                    alert("選択コースの変更が検出されたので、処理を停止します。");
+                    return;
+                }
                 const randomIndex = Math.floor(Math.random() * conditionList.length);
                 const imageId = conditionList[randomIndex];
                 //imageArea.src = `./image/mariokartcourse/${imageId}.jpg`;
