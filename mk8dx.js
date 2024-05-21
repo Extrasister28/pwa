a794c8acd0cafd145027ee1614304f3088934b21
MozukuDev <34203193+Extrasister28@users.noreply.github.com>
2024-05-21 20:23:06 +0900

    Update mk8dx.js

--- mk8dx.js
+++ mk8dx.js
@@ -8,7 +8,7 @@ function botanClick(){
     }
     document.getElementById('audiosaisei').currentTime = 0; //連続クリックに対応
     document.getElementById('audiosaisei').play(); //クリックしたら音を再生
-    for (let i = 0; i <= 20; i++) {
+    for (let i = 0; i <= 27; i++) {
         (function(i){
             setTimeout(function() {
                 if (conditionList.length === 0) {
