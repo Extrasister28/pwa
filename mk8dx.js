f4dd14f18c1daa84cb7aed9c503fe67b00cce46a
MozukuDev <34203193+Extrasister28@users.noreply.github.com>
2024-05-21 10:41:38 +0900

    Update mk8dx.js

--- mk8dx.js
+++ mk8dx.js
@@ -10,7 +10,7 @@ function botanClick(){
     const randomIndex = Math.floor(Math.random() * conditionList.length);
     const imageId = conditionList[randomIndex];
     //imageArea.src = `./image/mariokartcourse/${imageId}.jpg`;
-    document.getElementById('randcourse').innerHTML = `<img id="randimageArea" src="${imageId}.jpg" alt="Course" width="410" height="280" style="display: block; margin: auto;"/>`;
+    document.getElementById('randcourse').innerHTML = `<img id="randimageArea" src="./image/mariokartcourse/${imageId}.jpg" alt="Course" width="410" height="280" style="display: block; margin: auto;"/>`;
 }
 
 let button = document.getElementById('randbtn');
