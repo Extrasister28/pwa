3448abab3d853ad23e2f056af4201af49181791f
MozukuDev <34203193+Extrasister28@users.noreply.github.com>
2024-05-24 13:35:02 +0900

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -5,3 +5,14 @@ self.addEventListener('install', (event) => {
 self.addEventListener('activate', (event) => {
   event.waitUntil(self.clients.claim());
 });
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
