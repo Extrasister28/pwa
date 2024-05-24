0948b95bf2e5bfe2759d5c5ea016765811d46328
MozukuDev <34203193+Extrasister28@users.noreply.github.com>
2024-05-24 13:36:21 +0900

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -1,3 +1,24 @@
+const CACHE_NAME = 'extra-v1';
+
+caches.open(CACHE_NAME).then((cache) => {
+  return cache.addAll([
+    './',
+  ]);
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(event.request).then((response) => {
+        return response || fetch(event.request).then((response) => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
+
 self.addEventListener('install', (event) => {
    event.waitUntil(self.skipWaiting());
 });
