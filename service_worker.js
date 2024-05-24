6807ba31e3a5587a63621af9ca6887307b582217
MozukuDev <34203193+Extrasister28@users.noreply.github.com>
2024-05-24 13:39:35 +0900

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -1,28 +1,3 @@
-const CACHE_NAME = 'extra-v1';
+var cacheName = 'extra 0.0.1';
 
-caches.open(CACHE_NAME).then((cache) => {
-  return cache.addAll([
-    './',
-  ]);
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.match(event.request).then((response) => {
-        return response || fetch(event.request).then((response) => {
-          cache.put(event.request, response.clone());
-          return response;
-        });
-      });
-    })
-  );
-});
-
-self.addEventListener('install', (event) => {
-   event.waitUntil(self.skipWaiting());
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
-});
+// 以下の処理でこのキャッシュ名を使う
